faf9d69c3add46e4c3029989b619ff8eb04c04a4
bbbpppolly <bernard@weeklyhow.com>
2023-07-03 21:40:13 +0200

    Update the loading component

--- task-manager-app/client/src/components/ProgressBar.js
+++ task-manager-app/client/src/components/ProgressBar.js
@@ -1,31 +1,9 @@
-export default function ProgressBar({ progress }) {
-    const container = {
-        height: 20,
-        width: "100%",
-        backgroundColor: "#fff",
-        borderRadius: 50,
-        margin: 50
-    }
-     
-    const bar = {
-        height: "100%",
-        width: `${progress}%`,
-        backgroundColor: "#90CAF9",
-        borderRadius: "inherit",
-    }
-     
-    const label = {
-        padding: "1rem",
-        color: "#000000",
-    }
+export default function ProgressBar() {
 
     return (
-        <div className="ProgressBar" style={container}>
-            <div role="progressbar" style={bar}
-                 aria-valuenow={progress}
-                 aria-valuemin={0}
-                 aria-valuemax={100}>
-                <span style={label}>{`${progress}%`}</span>
+        <div>
+            <div>
+                <h2 className="text-xl">We are loading your tasks!</h2>
             </div>
         </div>
     );
