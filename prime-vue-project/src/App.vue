9adcaa707ddbbe40a086d1bbe18fce12dd2d7b68
Bernard Polidario <bernard@weeklyhow.com>
2025-06-21 15:14:56 +0200

    🎉 New: initialize prime-vue-project with Vite

---
+++ prime-vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue'
+import Button from 'primevue/button';
+</script>
+
+<template>
+  <main class="container">
+    <Navbar />
+
+    <Button label="Submit" />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ prime-vue-project/src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: "primevue",
+                order: "theme, base, primevue",
+            }
+        }
+    }
+});
+
+app.mount("#app")
