fca07381290ae747a665e457fb9ec0fa156168b1
Bernard Polidario <bernard@weeklyhow.com>
2025-06-27 11:01:53 +0200

    🎉initialize liquid-glass-vue project with Vite and Vue 3

---
+++ liquid-glass-vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AppCard from './components/AppCard.vue';
+import AppGlassmorphism from './components/AppGlassmorphism.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <AppGlassmorphism></AppGlassmorphism>
+    <AppCard>
+      <img class="dock-icon" src="/images/chrome.png" />
+      <img class="dock-icon" src="/images/netflix.png" />
+      <img class="dock-icon" src="/images/discord.png" />
+    </AppCard>
+  </main>
+</template>
+
+<style scoped>
+</style>
