2d04446a0159c163b064a3a5ef05f02d6fb452b4
Bernard Polidario <bernard@weeklyhow.com>
2025-10-24 14:01:48 +0200

    ✨ Add query suggestions plugin to Autocomplete component

--- weeklyhow-instantsearch/src/Autocomplete.tsx
+++ weeklyhow-instantsearch/src/Autocomplete.tsx
@@ -4,19 +4,25 @@ import { createRoot, Root } from "react-dom/client";
 import { usePagination, useSearchBox } from "react-instantsearch";
 import { autocomplete, AutocompleteOptions } from "@algolia/autocomplete-js";
 import { BaseItem } from "@algolia/autocomplete-core";
+import { createQuerySuggestionsPlugin } from "@algolia/autocomplete-plugin-query-suggestions";
 import { createLocalStorageRecentSearchesPlugin } from "@algolia/autocomplete-plugin-recent-searches";
 
 import "@algolia/autocomplete-theme-classic";
+import type { SearchClient } from "algoliasearch/lite";
 
 type AutocompleteProps = Partial<AutocompleteOptions<BaseItem>> & {
-  className?: string;
+    searchClient: SearchClient;
+    className?: string;
 };
 
 type SetInstantSearchUiStateOptions = {
   query: string;
 };
 
+const INSTANT_SEARCH_QUERY_SUGGESTIONS = "weeklyhow_videos_query_suggestions";
+
 export function Autocomplete({
+  searchClient,
   className,
   ...autocompleteProps
 }: AutocompleteProps) {
@@ -36,6 +42,32 @@ export function Autocomplete({
   }, [instantSearchUiState]);
 
   const plugins = useMemo(() => {
+    const querySuggestions = createQuerySuggestionsPlugin({
+      searchClient,
+      indexName: INSTANT_SEARCH_QUERY_SUGGESTIONS,
+      getSearchParams() {
+        return recentSearches.data!.getAlgoliaSearchParams({
+          hitsPerPage: 6,
+        });
+      },
+      transformSource({ source }) {
+        return {
+          ...source,
+          sourceId: "querySuggestionsPlugin",
+          onSelect({ item }) {
+            setInstantSearchUiState({ query: item.query });
+          },
+          getItems(params) {
+            if (!params.state.query) {
+              return [];
+            }
+
+            return source.getItems(params);
+          },
+        };
+      },
+    });
+
     const recentSearches = createLocalStorageRecentSearchesPlugin({
       key: "instantsearch",
       limit: 3,
@@ -49,7 +81,7 @@ export function Autocomplete({
       },
     });
 
-    return [recentSearches];
+    return [recentSearches, querySuggestions];
   }, [])
 
   useEffect(() => {
